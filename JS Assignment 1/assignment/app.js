d426776b86b0560fbe48da906da7bbd5dbe9eca0
Okaha Nadeem <okashanadeem0101@gmail.com>
2024-02-05 20:39:00 +0500

    Assignment-1
    JavaScript Assignment By Banoqabil

---
+++ JS Assignment 1/assignment/app.js
@@ -0,0 +1,157 @@
+// question: 01
+alert ("Welcome to Our Website");
+
+// question: 02
+alert ("Error! Please Enter a valid Password");
+
+// question: 03
+alert ("Hint : Use line break");
+
+// question: 04
+alert ("This is message 01");
+alert ("This is message 02");
+alert ("This is message 03");
+
+// question: 05
+// Make use of alerts in your new/existing HTML & CSS project. : DONE
+
+// question: 06
+// Done
+
+// question: 07
+var username
+
+// question: 08
+var myName = "Okasha Nadeem";
+
+// question: 09
+var  titledmessage = "Hello World";
+alert (titledmessage );
+
+// question: 10
+var studentsdata = "Okasha";"Shahzaib";"Armughan"
+alert (studentsdata)
+
+// question: 11
+var Email = "Okashanadeem0101@gmail.com"
+alert (Email)
+
+// question: 12
+
+// question: 13
+
+// question: 14
+var age = "17"
+alert (age)
+
+// question: 15
+var visit = "You have visited this site 5 times"
+alert (visit)
+
+// question: 16
+var birthYear = "2007"
+alert (birthYear)
+ 
+// question: 17
+var  Visitorname = prompt ("Entre your Name")
+var Producttitle = prompt ("Entre Product Name")
+var Quantity = prompt ("Entre he Quantity")
+console.log (Visitorname , "ordered" , Quantity , Producttitle, "from OkashaStore" )
+
+// question: 18
+var statement1 = "hello";
+var statement2 = "hy";
+var statement3 = "good night";
+
+// question: 19
+// Legal variable names in JavaScript must follow certain rules:
+
+// Legal Variable Names:
+
+// myVariable
+// _underscoreVariable
+// $dollarSignVariable
+// anotherVariable123
+// camelCaseVariable
+// Illegal Variable Names:
+
+// 123IllegalVariable (Cannot start with a number)
+// illegal-variable (Cannot contain hyphens)
+// illegal variable (Cannot contain spaces)
+// var (Reserved keyword)
+// function (Reserved keyword)
+
+// question: 21
+// Done in Html Body 
+
+// question: 21
+var num1 = (5)
+var num2 = (5)
+alert (num1 + num2)
+
+// question: 22
+var  subtractionnum1 = (5)
+var  subtractionnum2 = (5)
+alert ( subtractionnum1 + subtractionnum2)
+
+var multiplicationnum1 = (5)
+var multiplicationnum2 = (5)
+alert (multiplicationnum1 * multiplicationnum2)
+
+var divisionnum1 = (5)
+var divisionnum2 = (5)
+alert (divisionnum1 / divisionnum2)
+
+var modulusnum1 = (5)
+var modulusnum2 = (5)
+alert (modulusnum1 % modulusnum2)
+
+// question: 23
+// a. Declare a variable.
+var number= 5;
+
+// b. Show the value of the variable after declaration.
+console.log("Value after variable declaration is:", number);
+
+// c. Initialize the variable with some number.
+number = 5;
+
+// d. Show the initial value of the variable.
+console.log("Initial value:", number);
+
+// e. Increment the variable.
+number++;
+
+// f. Show the value of the variable after increment.
+console.log("Value after increment is:", number);
+
+// g. Add 7 to the variable.
+number += 7;
+
+// h. Show the value of the variable after addition.
+console.log("Value after addition is:", number);
+
+// i. Decrement the variable.
+number--;
+
+// j. Show the value of the variable after decrement.
+console.log("Value after decrement is:", number);
+
+// k. Show the remainder after dividing the variable's value by 3.
+var remainder = number % 3;
+
+// l. Output: "The remainder is: 0".
+console.log("The remainder is:", remainder);
+
+// question: 23
+var costofticket = 600
+console.log("price of 1 ticket is", costofticket)
+var buyingticket = 5
+console.log("number of buying tickets are", buyingticket)
+console.log ("price of 5 tickets is:")
+console.log(costofticket * buyingticket)
+
+// question: 23
+var value1 = prompt ("Entre your first Value")
+var value2 = prompt ("Entre your second Value")
+alert (value1 * value2)
